3de22a7c17ddd1f4d5aba064fa4da0d1ebe055ba
kausikhussain <kausik1027@gmail.com>
2026-04-02 01:56:44 +0530

    Day 17: Word Ladder (Graph)

---
+++ Day17/WordLadder.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class WordLadder {
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        Set<String> dict = new HashSet<>(wordList);
+        if (!dict.contains(endWord)) return 0;
+        Queue<String> queue = new LinkedList<>();
+        queue.offer(beginWord);
+        int level = 1;
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                String word = queue.poll();
+                char[] chars = word.toCharArray();
+                for (int j = 0; j < chars.length; j++) {
+                    char original = chars[j];
+                    for (char c = 'a'; c <= 'z'; c++) {
+                        if (c == original) continue;
+                        chars[j] = c;
+                        String newWord = new String(chars);
+                        if (newWord.equals(endWord)) return level + 1;
+                        if (dict.contains(newWord)) {
+                            queue.offer(newWord);
+                            dict.remove(newWord);
+                        }
+                    }
+                    chars[j] = original;
+                }
+            }
+            level++;
+        }
+        return 0;
+    }
+}
